e912fc4307a2628248991928564ed72f9ca87383
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-07-30 23:15:04 +0900

    Added XOPSFunctions.java and XOPSVersion.java.

---
+++ jxm-properties/src/main/java/com/github/dabasan/jxm/properties/xops/XOPSVersion.java
@@ -0,0 +1,11 @@
+package com.github.dabasan.jxm.properties.xops;
+
+/**
+ * XOPS version
+ * 
+ * @author Daba
+ *
+ */
+public enum XOPSVersion {
+	XOPS096, XOPS096T, XOPS097FT, XOPS0975T, XOPSOLT18F2, XOPSOLT19F2
+}
