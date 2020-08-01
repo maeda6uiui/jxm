19640ae2216e7f41507e0535f0e799259cdf07c6
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-01 14:08:13 +0900

    Updated test code for PD1Manipulator.

--- jxm-pd1/src/test/java/com/github/dabasan/jxm/pd1/PD1ManipulatorTest.java
+++ jxm-pd1/src/test/java/com/github/dabasan/jxm/pd1/PD1ManipulatorTest.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import com.github.dabasan.ejml_3dtools.Matrix;
-
 /**
  * Test class for PD1Manipulator
  * 
@@ -42,12 +40,58 @@ public class PD1ManipulatorTest {
 
 	@Test
 	public void testTransform() {
+		/*
 		var rotXMat = Matrix.createRotationXMatrix(Math.PI / 4.0);
 		var rotYMat = Matrix.createRotationYMatrix(Math.PI / 4.0);
 		var rotZMat = Matrix.createRotationZMatrix(Math.PI / 4.0);
 		var rotMat = rotZMat.mult(rotYMat).mult(rotXMat);
 		manipulator.transform(rotMat);
-
+		
 		manipulator.saveAsPD1("./Data/transform.pd1");
+		*/
+	}
+
+	@Test
+	public void testTranslate() {
+		// manipulator.translate(50.0, 50.0, 50.0);
+		// manipulator.saveAsPD1("./Data/translate.pd1");
+	}
+	@Test
+	public void testRotX() {
+		// manipulator.rotX(Math.PI / 4.0);
+		// manipulator.saveAsPD1("./Data/rotX.pd1");
+	}
+	@Test
+	public void testRotY() {
+		// manipulator.rotY(Math.PI / 4.0);
+		// manipulator.saveAsPD1("./Data/rotY.pd1");
+	}
+	@Test
+	public void testRotZ() {
+		// manipulator.rotZ(Math.PI / 4.0);
+		// manipulator.saveAsPD1("./Data/rotZ.pd1");
+	}
+	@Test
+	public void testRot() {
+		// manipulator.rot(0.0, 1.0, 0.0, Math.PI / 4.0);
+		// manipulator.saveAsPD1("./Data/rot.pd1");
+	}
+
+	@Test
+	public void testRescale() {
+		// manipulator.rescale(2.0, 2.0, 2.0);
+		// manipulator.saveAsPD1("./Data/rescale.pd1");
+	}
+
+	@Test
+	public void testRotateDirection() {
+		// manipulator.rotateDirection(Math.PI / 4.0);
+		// manipulator.saveAsPD1("./Data/rotateDirection.pd1");
+	}
+
+	@Test
+	public void testInvertZ() {
+		// manipulator.invertZ();
+		// manipulator.saveAsPD1("./Data/invertZ.pd1");
 	}
 }
