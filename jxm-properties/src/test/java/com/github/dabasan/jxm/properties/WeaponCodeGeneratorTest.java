12b5c3bf40b042074da9d268948e7224107f56f6
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-16 16:05:44 +0900

    Added test code for WeaponCodeGenerator.

---
+++ jxm-properties/src/test/java/com/github/dabasan/jxm/properties/WeaponCodeGeneratorTest.java
@@ -0,0 +1,43 @@
+package com.github.dabasan.jxm.properties;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.github.dabasan.jxm.properties.weapon.WeaponData;
+import com.github.dabasan.jxm.properties.weapon.openxops.WeaponCodeGenerator;
+import com.github.dabasan.jxm.properties.weapon.xgs.XGSManipulator;
+
+/**
+ * Test class for WeaponCodeGenerator
+ * 
+ * @author Daba
+ *
+ */
+public class WeaponCodeGeneratorTest {
+	private WeaponCodeGenerator generator;
+	private List<WeaponData> weapons;
+
+	public WeaponCodeGeneratorTest() {
+		generator = new WeaponCodeGenerator();
+
+		XGSManipulator xgsManipulator;
+		try {
+			xgsManipulator = new XGSManipulator("./Data/weapons.xgs");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		WeaponData[] weaponsArr = xgsManipulator.getWeaponData();
+		weapons = Arrays.asList(weaponsArr);
+	}
+
+	@Test
+	public void testGenerate() {
+		String code = generator.generate(weapons);
+		System.out.println(code);
+	}
+}
