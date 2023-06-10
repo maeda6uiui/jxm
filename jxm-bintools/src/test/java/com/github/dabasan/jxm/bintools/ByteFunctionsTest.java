00bc074d3cfbd174fc647d7588c5366d56291d00
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-10 10:53:40 +0900

    Use junit-jupiter-api

--- jxm-bintools/src/test/java/com/github/dabasan/jxm/bintools/ByteFunctionsTest.java
+++ jxm-bintools/src/test/java/com/github/dabasan/jxm/bintools/ByteFunctionsTest.java
@@ -1,8 +1,8 @@
 package com.github.dabasan.jxm.bintools;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test ByteFunctions
