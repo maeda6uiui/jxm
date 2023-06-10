de26b9233f56bc201c89b1315f76a65ed9ceeb01
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-06-10 09:07:32 +0900

    Update test code

--- jxm-properties/src/test/java/com/github/dabasan/jxm/properties/EXEManipulatorTest.java
+++ jxm-properties/src/test/java/com/github/dabasan/jxm/properties/EXEManipulatorTest.java
@@ -4,6 +4,7 @@ import com.github.dabasan.jxm.properties.xops.EXEManipulator;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -20,17 +21,29 @@ public class EXEManipulatorTest {
     }
 
     public EXEManipulatorTest() {
-        var srcFilepath = Paths.get(TARGET_DIR, "xops0975t.exe").toString();
-        try {
-            manipulator = new EXEManipulator(srcFilepath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        var srcFilenames = new ArrayList<String>();
+        srcFilenames.add("xops096.exe");
+        srcFilenames.add("xops096t.exe");
+        srcFilenames.add("xops097ft.exe");
+        srcFilenames.add("xops0975t.exe");
+        srcFilenames.add("xopsolt18f2.exe");
+        srcFilenames.add("xopsolt19f2.exe");
 
-        this.printSrcXOPSVersion();
-        this.printWeapons();
-        this.printCharacters();
-        this.write();
+        for (var srcFilename : srcFilenames) {
+            var srcFilepath = Paths.get(TARGET_DIR, srcFilename).toString();
+            try {
+                manipulator = new EXEManipulator(srcFilepath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            this.printSrcXOPSVersion();
+            this.printWeapons();
+            this.printCharacters();
+
+            String backupFilepath = Paths.get(TARGET_DIR, srcFilename + ".backup").toString();
+            this.write(srcFilepath, backupFilepath);
+        }
     }
 
     private void printSrcXOPSVersion() {
@@ -48,11 +61,8 @@ public class EXEManipulatorTest {
         Arrays.asList(manipulator.getCharacters()).forEach(System.out::println);
     }
 
-    private void write() {
+    private void write(String saveFilepath, String backupFilepath) {
         System.out.println("#write()");
-
-        var saveFilepath = Paths.get(TARGET_DIR, "xops0975t.exe").toString();
-        var backupFilepath = Paths.get(TARGET_DIR, "xops0975t_backup.exe").toString();
         manipulator.write(saveFilepath, backupFilepath);
     }
 }
